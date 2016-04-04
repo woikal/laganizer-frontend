5a34622f1c7f0ca59127cdaa2fd6c49c2afa5f4f
woikerl <alexander.woike@gmail.com>
2016-04-04 10:27:43 +0200

    add hrefs to nav

--- resources/templates/navbar.php
+++ resources/templates/navbar.php
@@ -1,11 +1,11 @@
 <ul class="wrapper">
-    <li>Start
-    </li><li>Timetable
-    </li><li>Tournaments
+    <li><a href="index.php">Start</a>
+    </li><li><a href="timetable.php">Timetable</a>
+    </li><li><a href="tournaments.php">Tournaments</a>
         <ul>
-            <li>Create Tournament
-            </li><li>Join Tounament
-            </li><li>Invite Players</li>
+            <li><a href="tournaments.php">Create Tournament</a>
+            </li><li><a href="tournaments.php">Join Tounament</a>
+            </li><li><a href="tournaments.php">Invite Players</a></li>
         </ul>
-    </li><li>Communicator</li>
+    </li><li><a href="chat.php">Chat</a></li>
 </ul>
